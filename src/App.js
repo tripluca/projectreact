b639530b492ccff8fa5f78f6a4d591bb55366534
tripluca <info@tripluca.com>
2021-02-15 10:06:59 +0200

    first commit online

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Tweet from "./Tweet";
 
 function App() {
+    const [users, setUsers] = useState([
+      {name: 'Ed', message: 'Hello there'},
+      {name: 'John', message: 'I am John Snow'},
+      {name: 'Bill', message: 'Cool'},
+     ]);
+ 
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+     {users.map(user =>
+     (
+      <Tweet name={user.name} message={user.message}/>
+
+     ))}
     </div>
   );
 }
